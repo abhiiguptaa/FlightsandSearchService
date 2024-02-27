422abbaba5ae06a6eb4cf9ebedf80db90f178bec
abhiiguptaa <abhishekgupta7554@gmail.com>
2024-02-27 16:40:00 +0000

    complete basic sequelize setup for create a db

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 // const port = 3000;
+const bodyParser = require("body-parser");
 const { port } = require("./config/serverConfig");
 
 require("dotenv").config();
 const startServer = async () => {
   const app = express();
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
   app.get("/", (req, res) => {
     res.send("hello wworld");
   });
